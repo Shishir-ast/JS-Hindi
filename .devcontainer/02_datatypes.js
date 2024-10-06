1a5cee40c5245cda8ba85554ec6ab99f3211bc9d
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-06 04:32:32 +0000

    02_datatypes.js

---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3); // code readability should be high
+
+   
+    console.log("Shishir");
+
+    let name = "Shishir";
+    let age = 18;
+    let isloggedin = false;
+    let state = null
+    
+
+// number =>2 to pwer 52
+//bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+
+
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof null)
