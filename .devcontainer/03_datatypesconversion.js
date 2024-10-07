80108bf3891d3571bb8bf56f090fcf61c896e653
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-07 04:31:57 +0000

    conversion of datatypes

---
+++ .devcontainer/03_datatypesconversion.js
@@ -0,0 +1,26 @@
+let score = "shishir";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score);
+//console.log( valueInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 ; false => 0
+
+//let isLoggedIn = "Shishir"
+
+//let BooleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log( BooleanIsLoggedIn);
+
+//1=> = true ; 0=> = false
+//"" => false
+// "Shishir" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
