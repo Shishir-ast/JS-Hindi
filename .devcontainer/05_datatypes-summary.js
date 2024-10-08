763c0af9a383520f3850a5ac89b5c21a5174a8c5
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-08 08:34:35 +0000

    datatypes summary

---
+++ .devcontainer/05_datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 103
+
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail =;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+//const bigNumber = 8472374234093284
+
+const bigNumber = 2372163721n
+
+// Reference ( Non primitive)
+
+//Array, Objects, Functions
+
+const heroes= ["shaktiman", "naagraaj", "doga"]
+
+let myObj = {
+name : "shishir"
+age : 22
+}
+
+const myFunction = function(){
+ console.log("Hello world")
+}
+console.log(typeof bigNumber);
