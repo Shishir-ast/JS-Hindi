4f8fb25ad3303ec16f3d4971328623a86428af89
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-08 07:43:07 +0000

    conversion rules

--- .devcontainer/03_datatypesconversion.js
+++ .devcontainer/03_datatypesconversion.js
@@ -20,7 +20,41 @@ let score = "shishir";
 //"" => false
 // "Shishir" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+let value = 3
+let negValue = - value
+//console.log(negValue)
+
+// console.log(2 + 2)
+// console.log)2 * 2)
+// console.log(2 ** 2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " shishir"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+// console.log("2" + 2)
+// console.log(1 + "2")
+// Watch datatypes conversion video
+// console.log(+true);
+// console.log(+"");
+
+
+// let num1, num2, num3
+
+//num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+//link to study https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
