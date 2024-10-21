1e639c3b624b5303f528961655ded22a38a54097
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-21 16:03:19 +0000

    tyes of datatypes

--- .devcontainer/datatypes-interview.js
+++ .devcontainer/datatypes-interview.js
@@ -2,17 +2,17 @@
 
 // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt, 
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail = undefined;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail = undefined;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 34780274629348940449243n
 
@@ -26,15 +26,42 @@ console.log(id === anotherId);
 
 // Array , Objects, Functions
 
-const heroes = ["shaktiman", "nagraj", "doga"]
+// const heroes = ["shaktiman", "nagraj", "doga"]
 
-let myObj = {
-    name : "shishir",
-    age: 22,
+// let myObj = {
+//     name : "shishir",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello Astha")
+// }
+
+// console.log(typeof outsideTemp)
+
+//*************************************************
+
+
+
+// Stack(Primitve), Heap (Reference)
+
+// let myYoutubeName = "shishirYt.com"
+
+// let anotherName = myYoutubeName
+
+// anotherName = "chaiaurcode"
+
+// console.log(myYoutubeName)
+// console.log(anotherName)
+
+let user1 = {
+    email: "user@google.vom",
+    upi:"123"
 }
 
-const myFunction = function(){
-    console.log("Hello Astha")
-}
+let user2 = user1
 
-console.log(typeof outsideTemp)
+user2.email = "shishir@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
