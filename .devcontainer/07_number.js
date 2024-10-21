cd90cd7b98bb1a621155209bf7f3f2beda337453
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-21 17:06:51 +0000

    mats and number

---
+++ .devcontainer/07_number.js
@@ -0,0 +1,33 @@
+// const score = 400;
+// const balance = new Number(500);
+// console.log(balance)
+// console.log(score);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//********************Maths*********************
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4,6));
+// console.log(Math.ceil(4,2))
+// console.log(Math.ceil(9,2))
+// console.log(Math.max(2,4,6))
+
+console.log((Math.random()*10) + 1)
+
+console.log((Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min +1 ))) +min )
