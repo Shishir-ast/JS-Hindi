ab01ffbc28293b9e9fd59cbb3d8ee099df630af6
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-22 17:03:59 +0000

    Array more

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,26 @@
+const marvel_Heroes = ["thor", 'spiderman', "iron man"]
+const dc_Heroes = ["batman", "Superman", "Wonderwoman"]
+
+// marvel_Heroes.push(dc_Heroes)
+
+// console.log(marvel_Heroes)
+// console.log(marvel_Heroes[3][1]);
+
+
+// const allHeroes = marvel_Heroes.concat(dc_Heroes)
+// console.log(allHeroes);
+
+// const all_NewHeroes = [...marvel_Heroes, ... dc_Heroes];
+// console.log(all_NewHeroes)
+
+// const another_Array = [1, 2, 3, 5, [6, 7 ,8 ], 0]
+
+// const real_Another_Array = another_Array.flat(Infinity)
+// console.log(real_Another_Array)
+
+
+// console.log(Array.isArray("Shishir"))
+// console.log(Array.from("shishir"))
+// console.log(Array.from({name: "shishir"}))    //interesting
+
+// console.log(Array.of(score1, score2, score3))
