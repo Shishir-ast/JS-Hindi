32c5d5b2d05f3965cce30e8543f178678ce9a2d0
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-22 16:40:00 +0000

    Array functions

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// Array
+
+const myArr = [ 1, 'a', 4, "b"];
+const myHeroes = [ "Shaktiman", "Nagraj"]
+
+console.log(myArr[1])
+console.log(myArr.length)
+
+// Array Methods
+
+// myArr.push(6)
+
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+
+
+// Slice, splice
+
+// console.log("A", myArr);
+
+// const myN1 = myArr.slice(1,3)
+// console.log(myN1)
+
+// const myN2 = myArr.splice(1,3)
+// console.log(myN2)
+// console.log(myN1)
