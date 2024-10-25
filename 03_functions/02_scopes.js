b8fc9edaa4537894fda9b28a1f8c7a256aa403ed
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-25 09:17:46 +0000

    scopes hosting

--- 03_functions/02_scopes.js
+++ 03_functions/02_scopes.js
@@ -1,13 +1,55 @@
-// var c = 300
-let a = 300
+// // var c = 300
+// let a = 300
 
-if(true){
-    let a = 10
-const b = 20
-var c = 30
+// if(true){
+//     let a = 10
+// const b = 20
+// var c = 30
+// }
+
+
+// // console.log(a);
+// // console.log(b);
+// console.log(a);
+
+// function one(){
+//     const username = "Sargam"
+
+//     function two(){
+//         const website = 'Youtube'
+//         console.log(username);
+ 
+//    }
+// //    console.log(website);
+
+//      two()
+// }
+
+// // one()
+
+// if (true){
+//     const username = "Maya"
+//     if(username === "Maya") {
+//         const website = " youtube"
+//         console.log(username + website)
+//     }
+//     //console.log(website);
+// }
+
+//console.log(username);
+
+// console.log(username)
+
+//++++++++++++++++++++++interesting+++++++++++++++++++
+addOne(5)
+
+function addOne(num){
+ return num +1
 }
 
 
-// console.log(a);
-// console.log(b);
-console.log(c);
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
