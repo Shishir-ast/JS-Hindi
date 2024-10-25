affa8411e571108a3c0dcb72fcdfb80caebdfe4a
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-25 11:37:37 +0000

    arrows

---
+++ 03_functions/03_arrows.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "Maya",
+    price : "Infinite",
+
+welcomeMessage : function(){
+    console.log(`${this.username}, welcome to the website`)
+    console.log(this);
+}
+
+
+}
+
+// user.welcomeMessage()
+// user.username = "Anshu"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai (){
+//     let username = "hitesh"
+//     console.log(this.username)
+// }
+// chai()
+
+// const chai =  ()=>{
+    
+//     let username = "hitesh"
+//     console.log(this)
+// }
+// // chai()
+
+// const addTwo = (num1,num2)=>{
+//     return num1 + num2
+// }
+// console.log(addTwo(2,4))
+
+// const addTwo = (num1,num2)=> num1 + num2
+// const addTwo = (num1,num2)=> (num1 + num2)
+
+
+
+console.log(addTwo(3,4))
+
+const myArray = [2, 5, 6 ,8]
+
+myArray.forEach()
