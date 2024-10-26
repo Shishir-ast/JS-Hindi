f61815ca2b0a005d0c30a5b7bf6b7fd72ef198b4
Shishir-ast <shishir1447@xavier.edu.np>
2024-10-26 16:01:36 +0000

    forEach

--- 05_iterations/FIVE.js
+++ 05_iterations/FIVE.js
@@ -1,3 +1,45 @@
 const coding = [ "js", "ruby", "java", "python", "cpp"]
 
-coding.forEach( function (va))
+// coding.forEach( function (item){ 
+//     console.log(item)
+// })
+
+// coding.forEach( (abc) => {
+//     console.log(abc)
+
+// } )
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) =>{
+//     console.log(item, index, arr);
+// } )
+
+
+const myCOding = [
+    {
+        languageName : "javascript",
+        languageFile : "JS"
+    },
+    {
+        languageName : "javascript",
+        languageFile : "JS"
+    },
+    {
+        languageName : "java",
+        languageFile : "java"
+    },
+    {
+        languageName : "python",
+        languageFile : "PY"
+    }
+]
+
+
+myCOding.forEach( (item) => {
+console.log(item.languageName);
+})
